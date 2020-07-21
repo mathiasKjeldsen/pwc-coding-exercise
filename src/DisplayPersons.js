5cb1d4cee3d2c31ae5a60088f30fa28e672b97e4
mathiasKjeldsen <mathias4040@hotmail.com>
2020-07-21 20:39:46 +0200

    Simple mapping

--- src/DisplayPersons.js
+++ src/DisplayPersons.js
@@ -3,6 +3,28 @@ import React from "react";
 export default class DisplayPersons extends React.Component {
   render() {
     const { persons } = this.props;
-    return <div />;
+    return (
+      <div>
+        {persons.map((person, key) => (
+          <div key={key} style={{ padding: "10px 0" }}>
+            <p>
+              {person.name}, {person.age}
+            </p>
+            {person.hobbies
+              ? person.hobbies.map((hobby, key) => (
+                  <p key={key} style={{ display: "inline" }}>
+                    {hobby +
+                      (person.hobbies.length === key + 1
+                        ? " "
+                        : person.hobbies.length === key + 2
+                        ? " and "
+                        : ", ")}
+                  </p>
+                ))
+              : "No hobbies"}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
